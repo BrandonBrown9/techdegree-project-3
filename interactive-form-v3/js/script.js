0e3545c56812621fff9a414af5e233a922f18545
bran9230 <branjay18@yahoo.com>
2020-12-01 18:46:42 -0500

    Added method to hide other input field

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -1,4 +1,6 @@
 // Global Variables
+const jobRole = document.getElementById("title");
+const otherJobRole = document.getElementById("other-job-role");
 
 // Methods
     //Focus on Input Field onPageLoad
@@ -7,4 +9,13 @@
         document.getElementById("name").focus();
     }
 
+    //Toggle Hidden Other Job Field
+    jobRole.addEventListener("change", function() {
+        if (jobRole.value == "other") {
+            otherJobRole.style.display = "block";
+        } else {
+            otherJobRole.style.display = "none";
+        }
+    })
+        
 // Method Calls
