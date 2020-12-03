c9a15acbae7da0edab498f221b697abc3d8eaecf
bran9230 <branjay18@yahoo.com>
2020-12-03 09:45:18 -0500

    Design Color Component v1

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -1,15 +1,16 @@
 // Global Variables
 const jobRole = document.getElementById("title");
 const otherJobRole = document.getElementById("other-job-role");
+const designEle = document.getElementById("design");
+const colorEle = document.getElementById("color");
 
 // Methods
     //Focus on Input Field onPageLoad
-
     function focusOnInput() {
         document.getElementById("name").focus();
     }
 
-    //Toggle Hidden Other Job Field
+    //Toggle Hidden Other Job Field Component
     jobRole.addEventListener("change", function() {
         if (jobRole.value == "other") {
             otherJobRole.style.display = "block";
@@ -17,5 +18,18 @@ const otherJobRole = document.getElementById("other-job-role");
             otherJobRole.style.display = "none";
         }
     })
-        
+    
+    //Design Theme and Color option component
+    colorEle.disabled = true;
+    designEle.addEventListener("change", function() {
+        //if statement can be rewritten to use case method
+        if (designEle.value == "js puns" ||  designEle.value == "heart js") {
+            colorEle.disabled = false;
+        } else {
+            colorEle.disabled = true;
+        }
+    })
+
 // Method Calls
+console.log(designEle);
+console.log(colorEle);
