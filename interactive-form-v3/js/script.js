df85dc9468c54bf797a1f631017cd6c4a1635bd0
bran9230 <branjay18@yahoo.com>
2020-12-07 16:02:51 -0500

    Working T-Shirt Design/Color Component Issue with initial selection remaining disabled when you choose a different Design

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -3,7 +3,9 @@ const jobRole = document.getElementById("title");
 const otherJobRole = document.getElementById("other-job-role");
 const designEle = document.getElementById("design");
 const colorEle = document.getElementById("color");
-const colorEleOption = document.querySelectorAll("#color option");
+const colorEleOption1 = document.getElementsByClassName("js puns");
+const colorEleOption2 = document.getElementsByClassName("heart js");
+
 
 // Methods
     //Focus on Input Field onPageLoad
@@ -23,35 +25,24 @@ const colorEleOption = document.querySelectorAll("#color option");
     //Design Theme and Color option component
     colorEle.disabled = true;
 
-    designEle.addEventListener("change", (e) => {
-        for (let i = 0; i < colorEle.length; i++) {
-            if (e.target.value === "js puns") {
+    designEle.addEventListener("change", function(color) {
+            if (designEle.value === "js puns") {
                 colorEle.disabled = false;
-                colorEleOption[0].style.display = "block";
-                colorEleOption[1].style.display = "block";
-                colorEleOption[2].style.display = "block";
-                colorEleOption[3].style.display = "none";
-                colorEleOption[4].style.display = "none";
-                colorEleOption[5].style.display = "none";
-            }  
-            
-            if (e.target.value === 'heart js') {
-                colorEle.disabled = false;
-                colorEleOption[0].style.display = "none";
-                colorEleOption[1].style.display = "none";
-                colorEleOption[2].style.display = "none";
-                colorEleOption[3].style.display = "block";
-                colorEleOption[4].style.display = "block";
-                colorEleOption[5].style.display = "block";
-            } 
+                for (let i = 0; i < colorEleOption2.length; i++) {
+                    colorEleOption2[i].disabled = true;
+                }
+            } else if (designEle.value === "heart js") {
+            colorEle.disabled = false;
+            for (let i = 0; i < colorEleOption1.length; i++) {
+                colorEleOption1[i].disabled = true;
+            }
         }
     })
 
 // Method Calls
 
-console.log(colorEleOption[1]);
 console.log(colorEle.length);
-
+// console.log(colorEleOption1);
 
 //two truths and a lie
     // I played electric guitar in a band and performed several times in Atlanta
