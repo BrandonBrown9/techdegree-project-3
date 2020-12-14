6d417a056e2599ae240a0f6fa305192e710f5c3b
bran9230 <branjay18@yahoo.com>
2020-12-14 13:24:57 -0500

    RegActivities Adding/Subtracting values

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -8,51 +8,54 @@ const colorEleOption2 = document.getElementsByClassName("heart js");
 const regActivities = document.getElementById("activities");
 const activitiesCost = document.getElementById("activities-cost");
 
-let totalCost = 0;
-
-
 // Methods
-    //Focus on Input Field onPageLoad
-    function focusOnInput() {
-        document.getElementById("name").focus();
+//Focus on Input Field onPageLoad
+function focusOnInput() {
+  document.getElementById("name").focus();
+}
+
+//Toggle Hidden Other Job Field Component
+jobRole.addEventListener("change", function () {
+  if (jobRole.value == "other") {
+    otherJobRole.style.display = "block";
+  } else {
+    otherJobRole.style.display = "none";
+  }
+});
+
+//Design Theme and Color option component
+colorEle.disabled = true;
+
+designEle.addEventListener("change", function (color) {
+  if (designEle.value === "js puns") {
+    colorEle.disabled = false;
+    for (let i = 0; i < colorEleOption2.length; i++) {
+      colorEleOption2[i].disabled = true;
+      colorEleOption1[i].disabled = false;
     }
+  } else if (designEle.value === "heart js") {
+    colorEle.disabled = false;
+    for (let i = 0; i < colorEleOption1.length; i++) {
+      colorEleOption1[i].disabled = true;
+      colorEleOption2[i].disabled = false;
+    }
+  }
+});
 
-    //Toggle Hidden Other Job Field Component
-    jobRole.addEventListener("change", function() {
-        if (jobRole.value == "other") {
-            otherJobRole.style.display = "block";
-        } else {
-            otherJobRole.style.display = "none";
-        }
-    })
-    
-    //Design Theme and Color option component
-    colorEle.disabled = true;
+//Register for Activities component
+regActivities.addEventListener("change", function (e) {
+  const dataCost = e.target;
+  const newDataCost = +dataCost.getAttribute("data-cost");
+  //   let newDataCost = +dataCost; // transforms dataCost STRING to newDataCost number
+  let totalCost = 0;
 
-    designEle.addEventListener("change", function(color) {
-            if (designEle.value === "js puns") {
-                colorEle.disabled = false;
-                for (let i = 0; i < colorEleOption2.length; i++) {
-                    colorEleOption2[i].disabled = true;
-                    colorEleOption1[i].disabled = false;
-                }
-            } else if (designEle.value === "heart js") {
-            colorEle.disabled = false;
-            for (let i = 0; i < colorEleOption1.length; i++) {
-                colorEleOption1[i].disabled = true;
-                colorEleOption2[i].disabled = false;
-            }
-        }
-    })
-
-    //Register for Activities component
-    regActivities.addEventListener("change", function (e) {
-        const dataCost = e.target.getAttribute('data-cost');
-            let newDataCost = +dataCost;
-        
-        //console.log(dataCost);
-    })
+  if (dataCost.checked === true) {
+    totalCost += newDataCost;
+    activitiesCost.textContent = totalCost;
+  } else {
+    totalCost -= newDataCost;
+    activitiesCost.textContent = totalCost;
+  }
+});
 
 // Method Calls
-
-    
